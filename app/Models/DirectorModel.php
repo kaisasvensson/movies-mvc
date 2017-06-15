d83f53223f3b07cfeeffb305f1df9fcb082369e5
kaisasvensson <kaisa.svensson@hotmail.com>
2017-06-15 21:15:21 +0200

    added a new model

---
+++ app/Models/DirectorModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaisa
+ * Date: 2017-06-15
+ * Time: 21:09
+ */
+
+namespace App\Models;
+
+
+class DirectorModel extends Model {
+
+        protected $table = 'directors';
+}
--- app/Models/UserModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-
-
-class UserModel extends Model {
-        protected $table = 'users';
-
-}
